a34aa7a04453ab3de30f43154dfda76640c78b94
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-07-24 21:47:12 +0530

    New commit

---
+++ Java_01_programme.java
@@ -0,0 +1,19 @@
+//Java is a object oriented programming language
+//Java use compiler and interpreter one by one to run
+//file name and class name alway try to write similar
+//class is basical a group of similar/related functions
+public class Java_01_programme{
+    //This is the main function
+    public static void main(String[] args){
+        //void main because it is dont return anything
+        System.out.println("Ahoy hoy universe"); //display this string in console
+    }
+    //this class is publick mean accessed in globaly
+}
+//for run this code write in terminal
+// javac {file_name}.java , in this case
+// javac Java_01_programme.java
+//which is make a class file
+//after compile it in terminal write
+// java Java_01_programme
+//output is executed in terminal
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Read the ./Java_supporting.txt of ths repo and then try to read the 
+codes and syntex here.
